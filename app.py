d9e0cc0b64db19e89a763ab9fc9ad739d602fa5d
Matheusaraujo007 <159270260+Matheusaraujo007@users.noreply.github.com>
2025-07-08 09:22:29 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -277,7 +277,8 @@ def export_pdf_status(status):
     return send_file(buffer, as_attachment=True, download_name=filename, mimetype="application/pdf")
 
 
-if __name__ == '__main__':
-    app.run(host='192.168.80.54', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
 
 
